a41985d31089acb6b91e69c91d770bcecc3f2089
Jaddy1 <jeffronaddy@gmail.com>
2019-04-17 02:51:28 -0400

    updated snap function

--- Relations.c
+++ Relations.c
@@ -4,77 +4,80 @@
 #include "Relations.h"
 #include "Database.h"
 
-
 CSG_Relation* new_CSG(char *C, int S, char *G) {
-    CSG_Relation *csg = malloc(sizeof(struct CSG));
+	CSG_Relation *csg = malloc(sizeof(struct CSG));
 
-    strcpy(csg->course, C);
-    csg->SID = S;
-    strcpy(csg->grade, C);
+	strcpy(csg->course, C);
+	csg->SID = S;
+	strcpy(csg->grade, C);
 
-    return csg;
+	return csg;
 }
 
 void insert_CSG(DB *db, CSG_Relation *csg) {
-	if(LinkedList_contains(db->csg, csg)) {
+	if (LinkedList_contains(db->csg, csg)) {
 		printf("ERROR: Duplicate tuple");
 	}
-
-LinkedList_add_at_end(db->csg, csg);
+	LinkedList_add_at_end(db->csg, csg);
 }
 
-void insert_SNAP(DB db, SNAP_Relation snap) {
-
+void insert_SNAP(DB *db, SNAP_Relation snap) {
+	if(LinkedList_contains(db->snap, snap)){
+		printf("ERROR: Duplicate tuple");
+	}else{
+		printf("Inserting snap in the database");
+		LinkedList_add_at_end(db->snap, snap);
+	}
 }
 
-void insert_CP(DB db, CP_Relation cp) {
-
-}
-
-void insert_CDH(DB db, CDH_Relation cdh) {
-
-}
-
-void insert_CR(DB db, CR_Relation cr) {
+void insert_CP(DB *db, CP_Relation cp) {
 
 }
 
-void delete_CSG(DB db, char *C, char *S, char *G) {
+void insert_CDH(DB *db, CDH_Relation cdh) {
 
 }
 
-void delete_SNAP(DB db, char *S, char *N, char *A, char *P) {
+void insert_CR(DB *db, CR_Relation cr) {
 
 }
 
-void delete_CP(DB db, char *C, char *P){
+void delete_CSG(DB *db, char *C, char *S, char *G) {
 
 }
 
-void delete_CDH(DB db, char *C, char *D, char *H){
+void delete_SNAP(DB *db, char *S, char *N, char *A, char *P) {
 
 }
 
-void delete_CR(DB db, char *C, char *R){
+void delete_CP(DB db, char *C, char *P) {
 
 }
 
-void lookup_CSG(DB db, char *C, char *S, char *G){
+void delete_CDH(DB db, char *C, char *D, char *H) {
 
 }
 
-void lookup_SNAP(DB db, char *S, char *N, char *A, char *P){
+void delete_CR(DB db, char *C, char *R) {
 
 }
 
-void lookup_CP(DB db, char *C, char *P){
+void lookup_CSG(DB db, char *C, char *S, char *G) {
 
 }
 
-void lookup_CDH(DB db, char *C, char *D, char *H){
+void lookup_SNAP(DB db, char *S, char *N, char *A, char *P) {
 
 }
 
-void lookup_CR(DB db, char *C, char *P){
+void lookup_CP(DB db, char *C, char *P) {
+
+}
+
+void lookup_CDH(DB db, char *C, char *D, char *H) {
+
+}
+
+void lookup_CR(DB db, char *C, char *P) {
 
 }
